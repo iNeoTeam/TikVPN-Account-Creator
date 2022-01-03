d8c59c5b247b25b6e02d9c9dfc7e9c1a9002a852
iNeoTeam | آی نئو <58592346+iNeoTeam@users.noreply.github.com>
2022-01-04 01:59:31 +0330

    CreateAccount PHP File

---
+++ CreateAccount.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Examples: 
+
+With Favorite Password:
+CreateAccount.php?email=example@server.com&password=TikVPN
+Without Password:
+CreateAccount.php?email=example@server.com
+*/
+error_reporting(0);
+header("content-type: application/json");
+function Random($len = 6){
+	$char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	$length = strlen($char); $random = '';
+	for($i = 0; $i < $len; $i++){ $random .= $char[rand(0, $length - 1)]; }
+	return $random;
+}
+$password = $_GET['password'];
+if($password == null){ $password = Random(); }
+$param = array('email' => $_GET['email'], 'password' => $password);
+$headers = array(
+	'Content-Type: application/json',
+	'login: '.base64_encode(json_encode($param))
+);
+$cURL = curl_init();
+curl_setopt($cURL, CURLOPT_URL, "https://api.ineo-team.ir/tikvpn.php");
+curl_setopt($cURL, CURLOPT_POST, 1);
+curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($cURL, CURLOPT_HTTPHEADER, $headers);
+$RESULT = curl_exec($cURL);
+curl_close ($cURL);
+echo $RESULT;
+?>
