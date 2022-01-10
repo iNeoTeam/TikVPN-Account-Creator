1910c23dc2e25abc629396c818aff9d795d6ca2e
iNeoTeam | آی نئو <58592346+iNeoTeam@users.noreply.github.com>
2022-01-10 10:22:07 +0330

    Update CreateAccount.php

--- CreateAccount.php
+++ CreateAccount.php
@@ -9,15 +9,16 @@ CreateAccount.php?email=example@server.com
 */
 error_reporting(0);
 header("content-type: application/json");
+$email = $_GET['email'];
+$password = $_GET['password'];
 function Random($len = 6){
 	$char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 	$length = strlen($char); $random = '';
 	for($i = 0; $i < $len; $i++){ $random .= $char[rand(0, $length - 1)]; }
 	return $random;
 }
-$password = $_GET['password'];
 if($password == null){ $password = Random(); }
-$param = array('email' => $_GET['email'], 'password' => $password);
+$param = array('email' => $email, 'password' => $password);
 $headers = array(
 	'Content-Type: application/json',
 	'login: '.base64_encode(json_encode($param))
